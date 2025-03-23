2e2d870bf3522a14df68206497f0e3e2330dedd4
ngan-dothuy <ngan.dothuy@nashtechglobal.com>
2025-03-23 14:03:51 +0700

    baitap_day07

---
+++ AutoTest/src/main/java/automation/testsuite/Employees.java
@@ -0,0 +1,34 @@
+package automation.testsuite;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class Employees {
+
+	@Test
+	public void enterEmployeeName() {
+		// TODO Auto-generated constructor stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of employees: ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+
+		String[] danhsachNv = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter employee name: ");
+			danhsachNv[i] = scanner.nextLine();
+		}
+
+		System.out.println("Employee name list: ");
+		for (String name : danhsachNv) {
+
+			System.out.println("-" + name);
+		}
+
+		scanner.close();
+
+	}
+
+}
--- AutoTest/src/main/java/automation/testsuite/LoginTest.java
+++ AutoTest/src/main/java/automation/testsuite/LoginTest.java
@@ -1,9 +1,17 @@
 package automation.testsuite;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
 
 	public LoginTest() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	
+	@Test
+	public void hello() {
+		System.out.println("Hello");
 	}
 
 }
